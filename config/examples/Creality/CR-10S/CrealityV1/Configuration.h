ebb19d0720ef247f7f3e8709f9a036fc64a4ab88
jholmstadt <john.holmstadt@nexteraanalytics.com>
2022-10-24 13:09:10 -0500

    My mods against 2.1.1

--- config/examples/Creality/CR-10S/CrealityV1/Configuration.h
+++ config/examples/Creality/CR-10S/CrealityV1/Configuration.h
@@ -611,7 +611,7 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 300	//MUCHTALL - Using a bi-metal heatbreak and a glass-encased thermistor capable of ~350C //#define HEATER_0_MAXTEMP 250
+#define HEATER_0_MAXTEMP 350	//MUCHTALL - Using a bi-metal heatbreak and a glass-encased thermistor capable of ~350C //#define HEATER_0_MAXTEMP 250
 #define HEATER_1_MAXTEMP 275
 #define HEATER_2_MAXTEMP 275
 #define HEATER_3_MAXTEMP 275
